4da55e63c8560908597bf8f79526bc033c808c59
Eric Kille <ekille@mac.com>
2011-12-27 20:57:53 -0500

    Initial Commit

---
+++ GrammerTrainer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GrammerTrainer
+//
+//  Created by Eric Kille on 12/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GrammerTrainer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GrammerTrainer
+//
+//  Created by Eric Kille on 12/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
